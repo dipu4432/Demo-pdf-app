7aa7f65c69e076daf30ac4f88621860146680802
Dipu Kumar <dipukumarpojhi@gmail.com>
2025-11-21 18:23:53 +0530

    Upload file added

--- src/inbox.tsx
+++ src/inbox.tsx
@@ -1,8 +1,21 @@
+function Inbox() {
+  const handleUpload = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (!file) return;
 
-const Inbox = () => {
-  return (
-    <div>Inbox</div>
-  )
+    const formData = new FormData();
+    formData.append("file", file);
+
+    const res = await fetch("http://localhost:5000/upload", {
+      method: "POST",
+      body: formData,
+    });
+
+    const data = await res.json();
+    console.log(data);
+  };
+
+  return <input type="file" onChange={handleUpload} />;
 }
 
-export default Inbox
+export default Inbox;
